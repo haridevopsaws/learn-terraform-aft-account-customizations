a50c2a4ecf03b15198d09ae5ed90a8f39b492dc4
Jinka <hari.prasad.jinka@accenture.com>
2025-01-28 15:08:46 +0530

    added bucket tag

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -3,4 +3,8 @@ data "aws_caller_identity" "current" {}
 resource "aws_s3_bucket" "sandbox_bucket" {
   bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
   acl    = "private"
+
+  tags = {
+    Name = "EISAI"
+  }
 }
