0d3161af4b734a2493af8bce0f58c8f398b6c49b
Hari <haricloud35@gmail.com>
2025-02-26 20:37:16 +0530

    s3 and vpc added

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -2,7 +2,7 @@ data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "sandbox_bucket" {
   bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
-  acl    = "private"
+  #acl    = "private"
 
   tags = {
     Name = "EISAI"
--- sandbox/terraform/vpc.tf
+++ sandbox/terraform/vpc.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-1"
 }
 resource "aws_vpc" "main" {
   cidr_block = "10.20.0.0/16"
