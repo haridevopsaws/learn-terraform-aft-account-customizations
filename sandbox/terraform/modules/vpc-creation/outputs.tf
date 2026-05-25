dc95157ed6242bb7e6a5e270bf2cbaf6278ca53f
haridevopsaws <80094546+haridevopsaws@users.noreply.github.com>
2026-05-25 23:30:43 +0530

    Delete sandbox/terraform/modules/vpc-creation directory

--- sandbox/terraform/modules/vpc-creation/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vpc_id" {
-  description = "VPC ID"
-  value       = aws_vpc.new_vpc.id
-}
-
-output "private_subnet_ids" {
-  description = "List of private subnet IDs"
-  value       = [for subnet in aws_subnet.private_subnet : subnet.id]
-}
