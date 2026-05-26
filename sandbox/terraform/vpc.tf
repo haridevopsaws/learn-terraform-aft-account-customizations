e497d64116ca28ff7cea5d7c2405db2e4c7efd44
haridevopsaws <80094546+haridevopsaws@users.noreply.github.com>
2026-05-26 19:59:57 +0530

    Update vpc.tf

--- sandbox/terraform/vpc.tf
+++ sandbox/terraform/vpc.tf
@@ -1,11 +1,20 @@
+variable "custom_fields" {
+  type = map(string)
+
+  default = {
+    create_vpc = "false"
+  }
+}
+
 locals {
-  create_vpc = var.custom_fields["create_vpc"] == "true"
+  create_vpc = lookup(var.custom_fields, "create_vpc", "false") == "true"
 }
 
 resource "aws_vpc" "main" {
   count = local.create_vpc ? 1 : 0
 
   cidr_block = "172.31.0.0/16"
+
   tags = {
     Name = "sandbox-vpc"
   }
