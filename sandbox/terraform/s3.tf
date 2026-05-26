a21861587e7d9eb5e9f604f254169336d712e1c9
haridevopsaws <80094546+haridevopsaws@users.noreply.github.com>
2026-05-26 20:24:36 +0530

    Update s3.tf

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -1,22 +1,10 @@
-data "aws_caller_identity" "current" {}
-
-resource "aws_s3_bucket" "sandbox_bucket" {
-  bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
-}
-
-resource "aws_s3_bucket_ownership_controls" "sandbox_bucket" {
-  bucket = aws_s3_bucket.sandbox_bucket.id
-
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
-resource "aws_s3_bucket_acl" "sandbox_bucket_acl" {
-  depends_on = [
-    aws_s3_bucket_ownership_controls.sandbox_bucket
-  ]
-
-  bucket = aws_s3_bucket.sandbox_bucket.id
-  acl    = "private"
+data "aws_caller_identity" "current" {} 
+resource "aws_s3_bucket" "sandbox_bucket" 
+{ 
+bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}" 
+} 
+resource "aws_s3_bucket_acl" "sandbox_bucket_acl" 
+{ 
+bucket = aws_s3_bucket.sandbox_bucket.id 
+acl = "private" 
 }
